eb6e68274ec5daa641dbcdb56802ea8ed4c6f711
Minh Nguyen Cong <mcong@box.com>
2022-11-01 19:39:13 +0100

    Added Theater, Room, Screening endpoints

--- src/configs/appConfig.ts
+++ src/configs/appConfig.ts
@@ -1,4 +1,4 @@
-import { Movie, MovieCategory, User } from 'src/@core/layouts/types'
+import { Movie, MovieCategory, Room, Screening, Seat, Theater, User } from 'src/@core/layouts/types'
 
 type AppConfig = {
   endpoint: string
@@ -99,6 +99,7 @@ export namespace MovieCategoryCreateEndpoint {
 
   export type Request = {
     name: string
+    slug: string
   }
   export type Response = {
     success: boolean
@@ -121,7 +122,9 @@ export namespace MovieCategoryGetEndpoint {
   export type Response = {
     success: boolean
     message: string
-    data?: MovieCategory
+    data?: MovieCategory & {
+      movies: Movie[]
+    }
   }
 }
 
@@ -178,7 +181,9 @@ export namespace MovieCreateEndpoint {
   export const path = '/movies'
   export const method = 'POST'
 
-  export type Request = Movie
+  export type Request = Movie & {
+    category: string[] // array of movie category ids
+  }
   export type Response = {
     success: boolean
     message: string
@@ -195,7 +200,9 @@ export namespace MovieUpdateEndpoint {
   export const path = '/movies/:id'
   export const method = 'PATCH'
 
-  export type Request = Movie
+  export type Request = Movie & {
+    category: string[] // array of movie category ids
+  }
   export type Response = {
     success: boolean
     message: string
@@ -232,7 +239,9 @@ export namespace MovieGetEndpoint {
   export type Response = {
     success: boolean
     message: string
-    data?: Movie
+    data?: Movie & {
+      category: MovieCategory
+    }
   }
 }
 
@@ -252,6 +261,315 @@ export namespace MovieUploadPosterEndpoint {
     success: boolean
     message: string
     data?: Movie
+  }
+}
+
+export namespace TheaterListEndpoint {
+  /**
+   * @description
+   * This endpoint is used to get list of theaters.
+   */
+  export const path = '/theaters'
+  export const method = 'GET'
+
+  export type Request = {}
+  export type Response = {
+    success: boolean
+    message: string
+    data?: Theater[]
+  }
+}
+
+export namespace TheaterCreateEndpoint {
+  /**
+   * @description
+   * This endpoint is used to create new theater.
+   *
+   */
+  export const path = '/theaters'
+  export const method = 'POST'
+
+  export type Request = Theater & {
+    managerId: string
+  }
+  export type Response = {
+    success: boolean
+    message: string
+    data?: Theater
+  }
+}
+
+export namespace TheaterGetEndpoint {
+  /**
+   * @description
+   * This endpoint is used to get theater.
+   * @param id - theater id
+   */
+  export const path = '/theaters/:id'
+  export const method = 'GET'
+
+  export type Request = {}
+  export type Response = {
+    success: boolean
+    message: string
+    data?: Theater & {
+      rooms: Room[]
+    }
+  }
+}
+
+export namespace TheaterUpdateEndpoint {
+  /**
+   * @description
+   * This endpoint is used to update theater.
+   * @param id - theater id
+   */
+  export const path = '/theaters/:id'
+  export const method = 'PATCH'
+
+  export type Request = Theater & {
+    managerId: string
+  }
+  export type Response = {
+    success: boolean
+    message: string
+    data?: Theater
+  }
+}
+
+export namespace TheaterDeleteEndpoint {
+  /**
+   * @description
+   * This endpoint is used to delete theater.
+   * @param id - theater id
+   */
+  export const path = '/theaters/:id'
+  export const method = 'DELETE'
+
+  export type Request = {}
+  export type Response = {
+    success: boolean
+    message: string
+  }
+}
+
+export namespace RoomListEndpoint {
+  /**
+   * @description
+   * This endpoint is used to get list of rooms.
+   */
+  export const path = '/rooms'
+  export const method = 'GET'
+
+  export type Request = {}
+  export type Response = {
+    success: boolean
+    message: string
+    data?: Room[]
+  }
+}
+
+export namespace RoomGetEndpoint {
+  /**
+   * @description
+   * This endpoint is used to get room.
+   * @param id - room id
+   */
+  export const path = '/rooms/:id'
+  export const method = 'GET'
+
+  export type Request = {}
+  export type Response = {
+    success: boolean
+    message: string
+    data?: Room & {
+      theater: Theater
+    }
+  }
+}
+
+export namespace RoomCreateEndpoint {
+  /**
+   * @description
+   * This endpoint is used to create new room.
+   * This also creates new seats for the room and assigns them to the room.
+   */
+  export const path = '/rooms'
+  export const method = 'POST'
+
+  export type Request = Room & {
+    theaterId: string
+  }
+  export type Response = {
+    success: boolean
+    message: string
+    data?: Room
+  }
+}
+
+export namespace RoomUpdateEndpoint {
+  /**
+   * @description
+   * This endpoint is used to update room.
+   * @param id - room id
+   *
+   * Only name can be updated.
+   */
+  export const path = '/rooms/:id'
+  export const method = 'PATCH'
+
+  export type Request = Room & {
+    theaterId: string
+  }
+  export type Response = {
+    success: boolean
+    message: string
+    data?: Room
+  }
+}
+
+export namespace RoomDeleteEndpoint {
+  /**
+   * @description
+   * This endpoint is used to delete room.
+   * @param id - room id
+   */
+
+  export const path = '/rooms/:id'
+  export const method = 'DELETE'
+
+  export type Request = {}
+  export type Response = {
+    success: boolean
+    message: string
+  }
+}
+
+export namespace SeatListEndpoint {
+  /**
+   * @description
+   * This endpoint is used to get list of seats for a room.
+   * @param roomId - room id
+   */
+  export const path = '/rooms/:roomId/seats'
+  export const method = 'GET'
+
+  export type Request = {}
+  export type Response = {
+    success: boolean
+    message: string
+    data?: Seat[]
+  }
+}
+
+export namespace SeatUpdateEndpoint {
+  /**
+   * @description
+   * This endpoint is used to update seat.
+   * @param seatId - seat id
+   */
+  export const path = '/seats/:seatId'
+  export const method = 'PATCH'
+
+  export type Request = Seat
+  export type Response = {
+    success: boolean
+    message: string
+    data?: Seat
+  }
+}
+
+export namespace ScreeningListEndpoint {
+  /**
+   * @description
+   * This endpoint is used to get list of screenings.
+   */
+  export const path = '/screenings'
+  export const method = 'GET'
+
+  export type Request = {}
+  export type Response = {
+    success: boolean
+    message: string
+    data?: Screening[]
+  }
+}
+
+export namespace ScreeningCreateEndpoint {
+  /**
+   * @description
+   * This endpoint is used to create new screening.
+   */
+
+  export const path = '/screenings'
+  export const method = 'POST'
+
+  export type Request = {
+    movieId: number
+    roomId: number
+    date: Date
+    openSale?: Date // Default is 7 days before screening date
+  }
+
+  export type Response = {
+    success: boolean
+    message: string
+    data?: Screening
+  }
+}
+
+export namespace ScreeningGetEndpoint {
+  /**
+   * @description
+   * This endpoint is used to get screening.
+   * @param id - screening id
+   */
+  export const path = '/screenings/:id'
+  export const method = 'GET'
+
+  export type Request = {}
+  export type Response = {
+    success: boolean
+    message: string
+    data?: Screening
+  }
+}
+
+export namespace ScreeningUpdateEndpoint {
+  /**
+   * @description
+   * This endpoint is used to update screening.
+   * @param id - screening id
+   */
+  export const path = '/screenings/:id'
+  export const method = 'PATCH'
+
+  export type Request = {
+    movieId?: number
+    roomId?: number
+    date?: Date
+    openSale?: Date
+  }
+  export type Response = {
+    success: boolean
+    message: string
+    data?: Screening
+  }
+}
+
+export namespace ScreeningDeleteEndpoint {
+  /**
+   * @description
+   * This endpoint is used to delete screening.
+   * @param id - screening id
+   */
+  export const path = '/screenings/:id'
+  export const method = 'DELETE'
+
+  export type Request = {}
+  export type Response = {
+    success: boolean
+    message: string
   }
 }
 

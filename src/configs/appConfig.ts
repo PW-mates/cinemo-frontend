c1a9b6c6802a1b64d2f148aca429ca5aca27f2ff
Minh Nguyen Cong <mcong@box.com>
2022-11-01 12:49:40 +0100

    Add movies endpoints

--- src/configs/appConfig.ts
+++ src/configs/appConfig.ts
@@ -1,4 +1,4 @@
-import { User } from 'src/@core/layouts/types'
+import { Movie, User } from 'src/@core/layouts/types'
 
 type AppConfig = {
   endpoint: string
@@ -34,6 +34,143 @@ export namespace AccountInfoEndpoint {
     success: boolean
     message: string
     data?: User
+  }
+}
+
+export namespace AccountUpdateEndpoint {
+  /**
+   * @description
+   * This endpoint is used to update user's account data.
+   */
+
+  export const path = '/account/info'
+  export const method = 'PATCH'
+
+  export type Request = User
+  export type Response = {
+    success: boolean
+    message: string
+    data?: User
+  }
+}
+
+export namespace AccountUpdatePasswordEndpoint {
+  /**
+   * @description
+   * This endpoint is used to update user's password.
+   */
+
+  export const path = '/account/password'
+  export const method = 'POST'
+
+  export type Request = {
+    oldPassword: string
+    newPassword: string
+  }
+  export type Response = {
+    success: boolean
+    message: string
+  }
+}
+
+export namespace MovieListEndpoint {
+  /**
+   * @description
+   * This endpoint is used to get list of movies.
+   */
+  export const path = '/movies'
+  export const method = 'GET'
+
+  export type Request = {}
+  export type Response = {
+    success: boolean
+    message: string
+    data?: Movie[]
+  }
+}
+
+export namespace MovieCreateEndpoint {
+  /**
+   * @description
+   * This endpoint is used to create new movie.
+   */
+  export const path = '/movies'
+  export const method = 'POST'
+
+  export type Request = Movie
+  export type Response = {
+    success: boolean
+    message: string
+    data?: Movie
+  }
+}
+
+export namespace MovieUpdateEndpoint {
+  /**
+   * @description
+   * This endpoint is used to update movie.
+   * @param id - movie id
+   */
+  export const path = '/movies/:id'
+  export const method = 'PATCH'
+
+  export type Request = Movie
+  export type Response = {
+    success: boolean
+    message: string
+    data?: Movie
+  }
+}
+
+export namespace MovieDeleteEndpoint {
+  /**
+   * @description
+   * This endpoint is used to delete movie.
+   * @param id - movie id
+   */
+  export const path = '/movies/:id'
+  export const method = 'DELETE'
+
+  export type Request = {}
+  export type Response = {
+    success: boolean
+    message: string
+  }
+}
+
+export namespace MovieGetEndpoint {
+  /**
+   * @description
+   * This endpoint is used to get movie.
+   * @param id - movie id
+   */
+  export const path = '/movies/:id'
+  export const method = 'GET'
+
+  export type Request = {}
+  export type Response = {
+    success: boolean
+    message: string
+    data?: Movie
+  }
+}
+
+export namespace MovieUploadPosterEndpoint {
+  /**
+   * @description
+   * This endpoint is used to upload movie poster.
+   * @param id - movie id
+   */
+  export const path = '/movies/:id/poster'
+  export const method = 'POST'
+
+  export type Request = {
+    poster: string // base64 encoded image
+  }
+  export type Response = {
+    success: boolean
+    message: string
+    data?: Movie
   }
 }
 

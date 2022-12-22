5920537f832767a97c25f0f24f72ab52abe22d69
AndriiDemk <andriidemydenkow@gmail.com>
2022-12-22 11:56:09 +0100

    init schedule page

---
+++ src/pages/schedule/index.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+
+import ScheduleCalendar from "src/views/showtimes/ScheduleCalendar";
+
+const Showtimes = () => {
+    return (
+        <Box>
+            hello world
+            <ScheduleCalendar />
+        </Box>
+    )
+}
+
+export default Showtimes;
---
+++ src/views/showtimes/ScheduleCalendar.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material"
+
+const ScheduleCalendar = () => {
+    return (
+        <Box>
+            ScheduleCalendar
+        </Box>
+    )
+}
+
+export default ScheduleCalendar

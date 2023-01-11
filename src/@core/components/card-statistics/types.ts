810edd139cd296da287dd66d9aa12ca380111837
Minh Nguyen Cong <mcong@box.com>
2023-01-11 17:48:08 +0100

    Update statistics

--- src/@core/components/card-statistics/types.ts
+++ src/@core/components/card-statistics/types.ts
@@ -6,10 +6,7 @@ import { ThemeColor } from 'src/@core/layouts/types'
 
 export type CardStatsVerticalProps = {
   title: string
-  stats: string
   icon: ReactNode
-  subtitle: string
   color?: ThemeColor
-  trendNumber: string
-  trend?: 'positive' | 'negative'
+  href: string
 }
